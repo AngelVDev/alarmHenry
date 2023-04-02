439fb8812a9903c898c782ee19aa694b22e07c15
Angel Scutari <antonioscutari2@hotmail.com>
2023-04-02 17:05:26 -0300

    Hispanoclocks

--- index.js
+++ index.js
@@ -1,68 +1,36 @@
-const display = document.getElementById('clock');
-
-const audio = new Audio('https://freesound.org/data/previews/251/251461_4146089-lq.mp3');
-const gold = new Audio("./golden_rule.mp3");
-const n = new Audio("./nani.mp3")
-audio.loop = true;
-
-let alarmTime = null;
-let alarmTimeout = null;
-
-
-function golden(){
-    gold.play()
+function display(clock) {
+  return document.getElementById(clock);
 }
 
-function updateTime() {
-    const date = new Date();
-
-    const hour = formatTime(date.getHours());
-    const minutes = formatTime(date.getMinutes());
-    const seconds = formatTime(date.getSeconds());
-    display.innerText=`${hour} : ${minutes} : ${seconds}`
+function updateTime(display, offset) {
+  const date = new Date();
+  const utc = date.getTime() + date.getTimezoneOffset() * 60000;
+  const localDate = new Date(utc + 3600000 * offset);
+  const hour = formatTime(localDate.getHours());
+  const minutes = formatTime(localDate.getMinutes());
+  const seconds = formatTime(localDate.getSeconds());
+  display.innerText = `${hour} : ${minutes} : ${seconds}`;
 }
 
 function formatTime(time) {
-    if ( time < 10 ) {
-        return '0' + time;
-    }
-    return time;
+  if (time < 10) {
+    return "0" + time;
+  }
+  return time;
 }
 
-function setAlarmTime(value) {
-    alarmTime = value
-    console.log(alarmTime)
-}
+const argClock = display("clockSelf");
+const chileClock = display("clockChile"); //-3
+const colClock = display("clockCol"); //-5
+const mexClock = display("clockMex"); //-6
+const venClock = display("clockVen"); //-4.5
+const perúClock = display("clockPer"); //-5
+const españaClock = display("clockEsp"); //+2
 
-function setAlarm() {
-    if(alarmTime) {
-        const current = new Date();
-        console.log(current)
-        const timeToAlarm = new Date(alarmTime);
-        console.log(timeToAlarm)
-
-        if (timeToAlarm > current) {
-            const timeout = timeToAlarm.getTime() - current.getTime();
-            alarmTimeout = setTimeout(() => audio.play(), timeout);
-            alert('Alarm set');
-        } else {
-            n.play()
-            alert('¿¿NANI??')
-        }
-    }
-}
-
-function HENRY(){
-    setTimeout(golden, 12 * 100000)
-    alert("Dis is Henry's golden rule ゴゴゴゴ")
-}
-
-function clearAlarm() {
-    audio.pause();
-    if (alarmTimeout) {
-        clearTimeout(alarmTimeout);
-        alert('Alarm cleared');
-    }
-}
-
-setInterval(updateTime, 1000);
+setInterval(() => updateTime(argClock, -3), 500);
+setInterval(() => updateTime(chileClock, -4), 500);
+setInterval(() => updateTime(colClock, -5), 500);
+setInterval(() => updateTime(mexClock, -6), 500);
+setInterval(() => updateTime(venClock, -4), 500);
+setInterval(() => updateTime(perúClock, -5), 500);
+setInterval(() => updateTime(españaClock, 2), 500);
